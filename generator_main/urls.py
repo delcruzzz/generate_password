22e1bd0437af6ab639fb8d46263788cc55460012
Leonado De la cruz <leonardodelacruz03@hotmail.com>
2022-09-13 21:26:07 -0500

    create password view a get request form

--- generator_main/urls.py
+++ generator_main/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('generate', views.generate, name="generate"),
+    path('password', views.password, name="password")
 ]
--- passwords_app/views.py
+++ passwords_app/views.py
@@ -6,3 +6,20 @@ def home(request):
 
 def generate(request):
     return render(request, "pages/generate.html")
+
+
+def password(request):
+
+    basic_characters = list("abcdefghijklmnopqrstuvwxyz")
+    generate_password = ""
+
+    ### Leer lo que viene a través de la query
+    lenght = int(request.GET.get("lenght"))
+
+    uppercase = request.GET.get("uppercase")
+
+    special = request.GET.get("special")
+
+    numbers = request.GET.get("numbers")
+
+    return render(request, "pages/password.html")
